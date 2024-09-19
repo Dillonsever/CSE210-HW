b846721f8b3b84feff2dbb9c58c606bf8d8101f9
dillonsever <dillonsever@gmail.com>
2024-09-19 16:29:17 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string user_grade = Console.ReadLine();
+        int number = int.Parse(user_grade);
+        string letter = "";
+
+        if ( number >= 90)
+            {
+                letter = "A";
+            }
+        else if (number >= 80 )
+            {
+
+                letter = "B";
+            }
+        else if (number >= 70 )
+            {
+                letter = "C";
+            }
+        else if (number >= 60 )
+            {
+                letter = "D";
+            }
+        else
+            {
+                letter = "F";
+            }
+
+        Console.WriteLine($"{letter}");
+
+        if ( number >= 70)
+        {
+            Console.WriteLine("Congrats you passed the class!!");
+        }
+        else
+        {
+            Console.WriteLine("Fail, Maybe Next Time");
+        }      
     }
 }
