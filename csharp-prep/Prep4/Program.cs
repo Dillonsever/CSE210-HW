2cdd80a4e86d64ffea7bcbbcf349350be501d6f9
dillonsever <dillonsever@gmail.com>
2024-09-26 01:52:12 -0600

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        int input = -1;
+        List<int> number = new List<int>(); 
+        Console.WriteLine("Enter a list of numbers and press 0 when done. ");
+
+        while(input != 0){
+            Console.WriteLine("Enter number: ");
+            string number_entered = Console.ReadLine();
+            input = int.Parse(number_entered); 
+            number.Add(input);
+
+        }
+
+        int sum = 0;
+        foreach (int num in number)
+        {
+            sum += num;
+        }
+        Console.WriteLine("The sum of the numbers is: " + sum);
+
+        if (number.Count > 0)
+        {
+            double average = number.Average();
+            Console.WriteLine("The average of the numbers is: " + average);
+        }
+
+        if (number.Count > 0)
+        {
+            int maxNumber = number.Max();
+            Console.WriteLine("The largest number is: " + maxNumber);
+        }
+
+
+            }
 }
